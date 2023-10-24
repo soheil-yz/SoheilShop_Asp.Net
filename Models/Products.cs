81863b306ee36204b0636a46aad324b8de681734
soheil yazdi1 <soheilyazdanii1382@gmail.com>
2023-10-24 19:24:20 +0330

    Create Product Models

---
+++ Models/Products.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+
+namespace SoheilShop.Models
+{
+    public class Products
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public List<Category> Categories { get; set; }
+
+        public Products()
+        {
+            Categories = new List<Category>();
+        }
+    }
+}
