95e29ff221b799fd340bc62249c85e6b10b79f3b
soheil yazdi1 <soheilyazdanii1382@gmail.com>
2023-10-24 19:27:26 +0330

    Create Item Model

---
+++ Models/Item.cs
@@ -0,0 +1,12 @@
+﻿namespace SoheilShop.Models
+{
+    public class Item
+    {
+        public int Id { get; set; }
+
+        public Products Products { get; set; }
+        public string Price { get; set; }
+        public int QuantityInStock {  get; set; }
+
+    }
+}
