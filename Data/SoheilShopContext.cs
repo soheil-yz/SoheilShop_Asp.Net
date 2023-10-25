e0459e383d190ebb36d07612a496b89006c2ec4b
soheil yazdi1 <soheilyazdanii1382@gmail.com>
2023-10-25 11:19:01 +0330

    Seet Data in DataBase.

--- Data/SoheilShopContext.cs
+++ Data/SoheilShopContext.cs
@@ -8,5 +8,32 @@ namespace SoheilShop.Data
         public SoheilShopContext(DbContextOptions<SoheilShopContext> options) : base(options) { }
 
         public DbSet<Category> category { get; set; }
+
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Category>().HasData(new Category()
+            {
+                Id = 1,
+                Name = "Soheil",
+                Description = "Description1"
+            }, new Category()
+            {
+                Id = 2,
+                Name = "لباس ورزشی ",
+                Description = "گروه لباس ورزشی"
+            }, new Category()
+            {
+                Id = 3,
+                Name = "ساعت مچی",
+                Description = "ساعت مچی"
+            }, new Category()
+            {
+                Id = 4,
+                Name = "لوازم خانگی",
+                Description = "لوازم خانگی"
+            });
+            base.OnModelCreating(modelBuilder);
+        }
     }
 }
---
+++ Migrations/20231025074617_second.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace SoheilShop.Migrations
+{
+    /// <inheritdoc />
+    public partial class second : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "category",
+                columns: new[] { "Id", "Description", "Name" },
+                values: new object[,]
+                {
+                    { 1, "Description1", "Soheil" },
+                    { 2, "گروه لباس ورزشی", "لباس ورزشی " },
+                    { 3, "ساعت مچی", "ساعت مچی" },
+                    { 4, "لوازم خانگی", "لوازم خانگی" }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "category",
+                keyColumn: "Id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "category",
+                keyColumn: "Id",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "category",
+                keyColumn: "Id",
+                keyValue: 3);
+
+            migrationBuilder.DeleteData(
+                table: "category",
+                keyColumn: "Id",
+                keyValue: 4);
+        }
+    }
+}
