4f3ac233bbb0cad5ca523c1c288526f89315c633
soheil yazdi1 <soheilyazdanii1382@gmail.com>
2023-10-25 10:59:04 +0330

    Add-First-Migration

---
+++ Data/SoheilShopContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SoheilShop.Models;
+
+namespace SoheilShop.Data
+{
+    public class SoheilShopContext : DbContext
+    {
+        public SoheilShopContext(DbContextOptions<SoheilShopContext> options) : base(options) { }
+
+        public DbSet<Category> category { get; set; }
+    }
+}
--- Startup.cs
+++ Startup.cs
@@ -1,9 +1,12 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.CodeAnalysis.Options;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using SoheilShop.Data;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -24,6 +27,13 @@ namespace SoheilShop
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllersWithViews();
+            #region Db Context
+            services.AddDbContext<SoheilShopContext>(Option =>
+            {
+                Option.UseSqlServer("Data Source=SOHEIL\\SQL2022;Initial Catalog=SoheilShop_DB;Integrated Security=true;TrustServerCertificate=True\r\n");
+            });                                                                                
+
+            #endregion
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
