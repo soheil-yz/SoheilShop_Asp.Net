04f6971f25a6ab6c7a20874aaeb1a5c0bf45a880
soheil yazdi1 <soheilyazdanii1382@gmail.com>
2023-10-27 22:01:13 +0330

    Create Relational With Component

---
+++ Controllers/ProductController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using SoheilShop.Data;
+using System.Linq;
+
+namespace SoheilShop.Controllers
+{
+    public class ProductController : Controller
+    {
+        private SoheilShopContext _context;
+        public ProductController(SoheilShopContext context)
+        {
+            _context = context;
+        }
+        [Route("Group/{id}/{name}")]
+        public IActionResult ShowProductByGroupId(int id, string name)
+        {
+            ViewData["GroupName"] = name;
+            var produts = _context.CategoryToProducts
+                .Where(c=>c.CategoryId == id)
+                .Include(c=>c.Products)
+                .Select(c=>c.Products)
+                .ToList();
+            return View(produts);
+        }
+    }
+}
